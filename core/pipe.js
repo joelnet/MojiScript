918357dd14ca89cabf991e66c1fa6fb7fedab8cd
Joel Thoms <jthoms@paciolan.com>
2018-09-05 10:41:39 -0700

    pipe interface change to accept array

--- core/pipe.js
+++ core/pipe.js
@@ -1,6 +1,6 @@
 const ensureExecutable = require('../internal/ensureExecutable')
 
-const pipe = (...xs) => value => xs.reduce(
+const pipe = (xs = []) => value => xs.reduce(
   (acc, x) => acc.then(ensureExecutable(x)),
   Promise.resolve(value)
 )
