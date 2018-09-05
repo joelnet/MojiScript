65e7d8a43329f19195b5b0cd92342ec258add5e7
Joel Thoms <jthoms@gmail.com>
2018-09-05 01:55:50 -0700

    refactor core/ifElse

--- core/ifElse.js
+++ core/ifElse.js
@@ -1,6 +1,6 @@
-const ensureExecutable = require('../internal/ensureExecutable')
+const maybeExec = require('../internal/maybeExec')
 
 const ifElse = test => t => f => value =>
-  ensureExecutable(test(value) ? t : f)(value)
+  maybeExec(test(value) ? t : f)(value)
 
 module.exports = ifElse
