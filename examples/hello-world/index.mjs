3582f3367913279463b95d7fedc35134958901b6
Joel Thoms <jthoms@gmail.com>
2018-09-04 22:33:27 -0700

    move files to root

---
+++ examples/hello-world/index.mjs
@@ -0,0 +1,13 @@
+//const log = require('joelscript/src/console/log')
+import log from 'joelscript/src/console/log'
+// import pipe from 'joelscript/core/pipe'
+// import run from 'joelscript/core/run'
+
+// const main = pipe(
+//   'Hello World',
+//   log
+// )
+
+// run({ main })
+
+console.log({ log })
