5290df1444deaec7d0183870859a278313189ef8
Joel Thoms <jthoms@gmail.com>
2018-09-05 22:11:42 -0700

    fix run tests

--- core/__tests__/run.test.js
+++ core/__tests__/run.test.js
@@ -3,54 +3,59 @@ const run = require('../run')
 describe('core/run', () => {
   test('runs main with no options', () => {
     const expected = null
-    const main = jest.fn()
+    const main = jest.fn().mockImplementation(() => Promise.resolve())
     run({ main })
     const actual = main.mock.calls[0][0]
-    expect(actual).toBe(expected)
+    return expect(actual).toBe(expected)
   })
 
   test('with no options returns value', () => {
+    expect.assertions(1)
     const expected = 888
-    const main = jest.fn().mockImplementation(() => expected)
+    const main = jest.fn().mockImplementation(() => Promise.resolve(expected))
     const actual = run({ main })
-    expect(actual).toBe(expected)
+    return expect(actual).resolves.toBe(expected)
   })
 
   test('with options returns value', () => {
+    expect.assertions(1)
     const expected = 888
     const options = expected
-    const main = jest.fn().mockImplementation(() => expected)
+    const main = jest.fn().mockImplementation(() => Promise.resolve(expected))
     const actual = run({ options, main })
-    expect(actual).toBe(expected)
+    return expect(actual).resolves.toBe(expected)
   })
 
   test('with dependencies sets dependencies', () => {
+    expect.assertions(1)
     const expected = 888
     const dependencies = expected
-    const main = jest.fn().mockImplementation(() => jest.fn())
+    const main = jest.fn().mockImplementation(() => jest.fn().mockImplementation(() => Promise.resolve(expected)))
     run({ dependencies, main })
     const actual = main.mock.calls[0][0]
-    expect(actual).toBe(expected)
+    return expect(actual).toBe(expected)
   })
 
   test('with dependencies and options sets options', () => {
+    expect.assertions(1)
     const expected = 888
     const options = expected
     const dependencies = 8
-    const main = jest.fn()
+    const main = jest.fn().mockImplementation(() => Promise.resolve(expected))
     const deps = jest.fn().mockImplementation(() => main)
     run({ dependencies, options, main: deps })
     const actual = main.mock.calls[0][0]
-    expect(actual).toBe(expected)
+    return expect(actual).toBe(expected)
   })
 
   test('with dependencies and options returns value', () => {
+    expect.assertions(1)
     const expected = 888
     const options = expected
     const dependencies = 8
-    const main = jest.fn().mockImplementation(() => expected)
+    const main = jest.fn().mockImplementation(() => Promise.resolve(expected))
     const deps = jest.fn().mockImplementation(() => main)
     const actual = run({ dependencies, options, main: deps })
-    expect(actual).toBe(expected)
+    return expect(actual).resolves.toBe(expected)
   })
 })
