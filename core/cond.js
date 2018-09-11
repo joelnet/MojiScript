dacad3102581780850812284704e8a6cd75616e0
Joel Thoms <jthoms@paciolan.com>
2018-09-11 13:03:34 -0700

    remove isFunction in favor of just is.

--- core/cond.js
+++ core/cond.js
@@ -1,5 +1,7 @@
 const maybeExec = require('../internal/maybeExec')
-const isFunction = require('../is/isFunction')
+const is = require('../types/is')
+
+const isFunction = is(Function)
 
 const isPass = (test, value) =>
   isFunction(test) ? test(value) : test === value
@@ -7,7 +9,7 @@ const isPass = (test, value) =>
 const cond = xs => value => {
   for (let i = 0; i < xs.length; i++) {
     const [test, result] = xs[i]
-    const pass = isFunction(test) ? test(value) : test === value
+
     if (isPass(test, value)) {
       return maybeExec(result)(value)
     }
