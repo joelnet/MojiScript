a819e9fa4fe64aed47695d06a5f8b7d1141e8245
Joel Thoms <jthoms@paciolan.com>
2018-09-12 11:05:39 -0700

    update string/template to work with Array.prototype.map

--- string/__tests__/template.test.js
+++ string/__tests__/template.test.js
@@ -1,17 +1,32 @@
-const template = require('../template')
+const $ = require('../template')
 
 describe('string/template', () => {
+  const list = [
+    { foo: 'World' },
+    { foo: 'Sunshine' }
+  ]
+
   test('simple template', () => {
     const expected = 'YAY!'
-    const func = template`${0}${1}${0}!`
+    const func = $`${0}${1}${0}!`
     const actual = func('Y', 'A')
     expect(actual).toBe(expected)
   })
 
-  test('simple template', () => {
+  test('multiple template', () => {
     const expected = 'Hello World!'
-    const func = template`${0} ${'foo'}!`
-    const actual = func('Hello', { foo: 'World' })
+    const func = $`${1} ${'foo'}!`
+    const actual = func({ foo: 'World' }, 'Hello')
     expect(actual).toBe(expected)
+  })
+
+  test('map template', () => {
+    const expected = [
+      'Hello World @ index 0!',
+      'Hello Sunshine @ index 1!'
+    ]
+    const tem = $`Hello ${'foo'} @ index ${1}!`
+    const actual = list.map(tem)
+    expect(actual).toMatchObject(expected)
   })
 })
