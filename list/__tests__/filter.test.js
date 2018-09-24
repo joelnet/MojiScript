8ba0bb20e1ee746ac241241a5b837b34b164a841
Joel Thoms <jthoms@paciolan.com>
2018-09-24 16:01:27 -0700

    add async support to map, filter, reduce

--- list/__tests__/filter.test.js
+++ list/__tests__/filter.test.js
@@ -1,11 +1,19 @@
-const filter = require('../filter')
+const filter = require("../filter")
 
-describe('list/filter', () => {
+describe("list/filter", () => {
   const isOdd = num => num % 2 !== 0
+  const asyncIsOdd = num => Promise.resolve(num).then(isOdd)
 
-  test('filters', () => {
+  test("sync filters", () => {
     const expected = [1, 3]
-    const actual = filter (isOdd) (expected)
+    const actual = filter(isOdd)([1, 2, 3])
     expect(actual).toMatchObject(expected)
+  })
+
+  test("async filters", () => {
+    expect.assertions(1)
+    const expected = [1, 3]
+    const actual = filter(asyncIsOdd)([1, 2, 3])
+    return expect(actual).resolves.toMatchObject(expected)
   })
 })
