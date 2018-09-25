dea5105481b12a74d73edc03409e767cc4db8b53
Joel Thoms <jthoms@paciolan.com>
2018-09-25 11:51:20 -0700

    add list/range. update fizz-buzz example

---
+++ list/__tests__/range.test.js
@@ -0,0 +1,9 @@
+const range = require('../range')
+
+describe('list/range', () => {
+  test('creates range', () => {
+    const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const actual = [...range(0)(10)]
+    expect(actual).toMatchObject(expected)
+  })
+})
