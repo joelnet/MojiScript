2a3f809d51a8f0441f2e6175be377601f893ac38
Joel Thoms <jthoms@paciolan.com>
2018-10-04 10:54:01 -0700

    update FizzBuzz to be consistent with article.

--- examples/fizz-buzz/main.mjs
+++ examples/fizz-buzz/main.mjs
@@ -1,40 +1,28 @@
+import cond from 'mojiscript/logic/cond'
 import pipe from 'mojiscript/core/pipe'
 import map from 'mojiscript/list/map'
 import range from 'mojiscript/list/range'
 import allPass from 'mojiscript/logic/allPass'
-import cond from 'mojiscript/logic/cond'
 
-// getFizzInfo :: Number -> FizzInfo
-const getFizzInfo = value => ({
-  value,
-  fizz: value % 3 === 0,
-  buzz: value % 5 === 0
-})
+const isFizz = num => num % 3 === 0
+const isBuzz = num => num % 5 === 0
+const isFizzBuzz = allPass ([ isFizz, isBuzz ])
 
-// isFizz :: FizzInfo -> Boolean
-const isFizz = ({ fizz }) => fizz
-
-// isBuzz :: FizzInfo -> Boolean
-const isBuzz = ({ buzz }) => buzz
-
-// fizzInfoToStatus :: FizzInfo -> String | Number
-const fizzInfoToStatus = cond ([
-  [ allPass ([ isFizz, isBuzz ]), 'FizzBuzz' ],
+const fizziness = cond ([
+  [ isFizzBuzz, 'FizzBuzz' ],
   [ isFizz, 'Fizz' ],
   [ isBuzz, 'Buzz' ],
-  [ () => true, ({ value }) => value ]
+  [ () => true, x => x ]
 ])
 
-// fizzBuss :: Number -> String | Number
-const fizzBuzz = pipe ([
-  getFizzInfo,
-  fizzInfoToStatus
+const logFizziness = log => pipe ([
+  fizziness,
+  log
 ])
 
 const main = ({ log }) => pipe ([
   ({ start, end }) => range (start) (end + 1),
-  map (fizzBuzz),
-  map (log)
+  map (logFizziness (log))
 ])
 
 export default main
