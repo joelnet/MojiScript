28d9cc83b3f7267a12983040be118899bad0df79
Joel Thoms <jthoms@gmail.com>
2018-10-10 21:55:10 -0700

    tap modified to support async functions.
    fixes: https://github.com/joelnet/MojiScript/issues/146

--- function/__tests__/tap.test.js
+++ function/__tests__/tap.test.js
@@ -9,6 +9,14 @@ describe('function/tap', () => {
     expect(actual).toBe(expected)
   })
 
+  test('async returns original argument', () => {
+    const func = () => Promise.resolve(666)
+    const tapped = tap(func)
+    const expected = 888
+    const actual = tapped(expected)
+    expect(actual).resolves.toBe(expected)
+  })
+
   test('returns executes function with argument', () => {
     const func = jest.fn()
     const tapped = tap(func)
@@ -18,5 +26,21 @@ describe('function/tap', () => {
     const actual = func.mock.calls[0][0]
 
     expect(actual).toBe(expected)
+  })
+
+  test('exception bubbles up', () => {
+    const expected = Error('Oops')
+    const func = () => { throw Error('Oops') }
+    const tapped = tap(func)
+    const actual = () => tapped(expected)
+    expect(actual).toThrowError(expected)
+  })
+
+  test('async exception bubbles up', () => {
+    const expected = Error('Oops')
+    const func = () => Promise.reject(expected)
+    const tapped = tap(func)
+    const actual = tapped(888)
+    expect(actual).rejects.toBe(expected)
   })
 })
