b43ca2bc615dbc98bd7e72d42d5a76ea93325adc
Corned <oliver@kaarnalehto.fi>
2018-10-13 22:43:43 +0300

    streamlined the sort function

--- list/sort.js
+++ list/sort.js
@@ -1,15 +1,6 @@
 /* eslint-disable */
 const sort = func => iterable => {
-  const values = []
-  const iterator = iterable[Symbol.iterator]()
-  var { value, done } = iterator.next()
-
-  while (!done) {
-    values.push(value)
-    var { value, done } = iterator.next()
-  }
-
-  return values.sort((a, b) => func(a) (b))
+  return [...iterable].sort((a, b) => func (a) (b))
 }
 
 module.exports = sort
