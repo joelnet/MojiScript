615f72f55863fa5ef347dec13cc5c9af52d65ea0
Joel Thoms <jthoms@gmail.com>
2018-10-14 19:43:16 -0700

    add tests for reduce

--- list/__tests__/reduce.test.js
+++ list/__tests__/reduce.test.js
@@ -1,37 +1,56 @@
-const reduce = require("../reduce")
+const range = require('../range')
+const reduce = require('../reduce')
 
-describe("list/reduce", () => {
+describe('list/reduce', () => {
+  const isEven = num => num % 2 === 0
   const add = x => y => x + y
-  const asyncAdd = x => y => Promise.resolve(x).then(add(y))
-  function* iterator() {
-    yield 1
-    yield 2
-    yield 3
-  }
+  const asyncAdd = x => y => Promise.resolve(y).then(add(x))
+  const asyncWhenEvenAdd = x => y => isEven(y) ? asyncAdd(x)(y) : add(x)(y)
 
-  test("sync array", () => {
+  test('sync array', () => {
     const expected = 6
     const actual = reduce(add)(0)([1, 2, 3])
     expect(actual).toBe(expected)
   })
 
-  test("async array", () => {
+  test('async array', () => {
     expect.assertions(1)
     const expected = 6
     const actual = reduce(asyncAdd)(0)([1, 2, 3])
     return expect(actual).resolves.toBe(expected)
   })
 
-  test("sync iterable", () => {
+  test('sync iterable', () => {
     const expected = 6
-    const actual = reduce(add)(0)(iterator())
+    const actual = reduce(add)(0)(range(1)(4))
     expect(actual).toBe(expected)
   })
 
-  test("async iterable", () => {
+  test('async iterable', () => {
     expect.assertions(1)
     const expected = 6
-    const actual = reduce(asyncAdd)(0)(iterator())
+    const actual = reduce(asyncAdd)(0)(range(1)(4))
+    return expect(actual).resolves.toBe(expected)
+  })
+
+  test('async array mixed', () => {
+    expect.assertions(1)
+    const expected = 6
+    const actual = reduce(asyncWhenEvenAdd)(0)([1, 2, 3])
+    return expect(actual).resolves.toBe(expected)
+  })
+
+  test('async iterable mixed', () => {
+    expect.assertions(1)
+    const expected = 6
+    const actual = reduce(asyncWhenEvenAdd)(0)([1, 2, 3])
+    return expect(actual).resolves.toBe(expected)
+  })
+
+  test('async iterable mixed', () => {
+    expect.assertions(1)
+    const expected = 6
+    const actual = reduce(asyncWhenEvenAdd)(0)(range(0)(4))
     return expect(actual).resolves.toBe(expected)
   })
 })
