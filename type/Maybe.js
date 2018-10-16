7f4ec3cc1e9f83e01ce8633b01365ac77900e277
joelnet <jthoms@gmail.com>
2018-10-16 16:57:38 -0700

    add support for is (Maybe)

--- type/Maybe.js
+++ type/Maybe.js
@@ -2,10 +2,12 @@ const is = require('./is')
 const Just = require('./Just')
 const Nothing = require('./Nothing')
 
+const type = Symbol('mojiscript/type/Maybe')
 const fromMaybe = maybe => is(Just)(maybe) ? maybe.value : null
 const toMaybe = value => value == null ? Nothing : Just(value)
 
 module.exports = {
+  '@@type': type,
   fromMaybe,
   toMaybe
 }
