2f27abf13194a5b709f1f59445ea7791995b7d30
Tryggvi Gylfason <tryggvigy@gmail.com>
2018-10-29 23:36:48 +0100

    Implement object/pathOr

---
+++ object/pathOr.js
@@ -0,0 +1,15 @@
+const pathOr = fallback => path => (o) => {
+  if (!path.length) return fallback
+
+  /* eslint-disable no-restricted-syntax */
+  for (const key of path) {
+    if (!o || !Object.prototype.hasOwnProperty.call(o, key)) {
+      return fallback
+    }
+    // eslint-disable-next-line no-param-reassign
+    o = o[key]
+  }
+  return o
+}
+
+module.exports = pathOr
