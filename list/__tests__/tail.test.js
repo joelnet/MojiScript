86238187e4b8cae1e7590e95c9484184dedb714e
Joel Thoms <jthoms@gmail.com>
2018-12-06 21:03:03 -0800

    bug fixes for list/tail

--- list/__tests__/tail.test.js
+++ list/__tests__/tail.test.js
@@ -3,6 +3,7 @@ const range = require('../range')
 
 describe('list/tail', () => {
   test('tail of array', () => {
+    expect.assertions(1)
     const expected = [ 2, 3 ]
     const actual = tail([ 1, 2, 3 ])
 
@@ -10,6 +11,7 @@ describe('list/tail', () => {
   })
 
   test('tail of range', () => {
+    expect.assertions(1)
     const expected = [ 4, 5, 6 ]
     const actual = tail(range(3)(7))
 
@@ -17,18 +19,16 @@ describe('list/tail', () => {
   })
 
   test('tail of null is not supported', () => {
-    try {
-      tail(null)
-    } catch (error) {
-      expect(error.message).toBe('type object is not supported')
-    }
+    expect.assertions(1)
+    const expected = Error('type object is not supported')
+    const actual = () => tail(null)
+    expect(actual).toThrow(expected)
   })
 
   test('tail of object is not supported', () => {
-    try {
-      tail({})
-    } catch (error) {
-      expect(error.message).toBe('type object is not supported')
-    }
+    expect.assertions(1)
+    const expected = Error('type object is not supported')
+    const actual = () => tail({})
+    expect(actual).toThrow(expected)
   })
 })
