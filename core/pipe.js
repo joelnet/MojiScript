65818a94a36d5b890f2bd0b828630956b435dc9d
Joel Thoms <jthoms@paciolan.com>
2019-02-06 11:02:41 -0800

    feat(pipe): pipe also sync
    BREAKING CHANGE: pipe will not always return a Promise, previously it would.

--- core/pipe.js
+++ core/pipe.js
@@ -1,8 +1,9 @@
 const maybeExec = require('../_internal/maybeExec')
+const call = require('../_internal/call')
 
 const pipe = (funcs = []) => value => funcs.reduce(
-  (acc, func) => acc.then(maybeExec(func)),
-  Promise.resolve(value)
+  (acc, func) => call(maybeExec(func))(acc),
+  value
 )
 
 module.exports = pipe
