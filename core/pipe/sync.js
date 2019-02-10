130199d0ea8a86c75dd1179ad525cf5266bec7e4
Joel Thoms <jthoms@gmail.com>
2019-02-09 18:13:34 -0800

    core/sync to throw when no args

--- core/pipe/sync.js
+++ core/pipe/sync.js
@@ -1,8 +1,11 @@
 const maybeExec = require('../../_internal/maybeExec')
 
-const pipe = (funcs = []) => value => funcs.reduce(
-  (acc, func) => maybeExec(func)(acc),
-  value
-)
+const pipe = (funcs = []) =>
+  funcs.length === 0
+    ? (() => { throw Error('pipe/sync requires at least one argument') })()
+    : value => funcs.reduce(
+      (acc, func) => maybeExec(func)(acc),
+      value
+    )
 
 module.exports = pipe
